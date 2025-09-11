99c21a62153cfbe603491923b40f7fe1b21ae246
Audrey <>
2025-09-11 16:15:46 +0200

    Initial commit of the project

--- server/index.ts
+++ server/index.ts
@@ -60,10 +60,12 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  // changement du port 5000 pour 3000 pour le faire tourner en local
+  const port = parseInt(process.env.PORT || '3000', 10); 
   server.listen({
     port,
-    host: "0.0.0.0",
+    // host: "0.0.0.0", changement d'adresse pour le faire tourner en local
+    host: "127.0.0.1",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
